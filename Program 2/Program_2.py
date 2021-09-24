339dddc1fde56de1229ec0291806b3d5d5d18b0e
findme-out <89270561+findme-out@users.noreply.github.com>
2021-09-24 14:45:20 +0300

    Add files via upload
    Добавлено разделение на енг и ру подгруппы (в соответствии с true и false соотвественно в пункте 1.3)
    Реструктурирована генерация статистики в отдельные функции (gen_statistics_1, gen_statistics_2), возвращающие словарь, необходимый для последующей генерации html страницы (все будущие функции статистики тоже будут в отдельных функциях)

--- Program 2/Program_2.py
+++ Program 2/Program_2.py
@@ -23,8 +23,39 @@ def generate_html(output_file_name, path_to_csv, template_params, template_name=
         temp_file_name = output_file_name + '_' + str(index)
         index += 1
     with open(path + temp_file_name + '.html', 'w', encoding='utf-8') as file:
-        file.write(template(title=template_params['title'][0], col_title=template_params['col_title'], row_num=template_params['row_num'],
+        file.write(template(title=template_params['title'], col_title=template_params['col_title'], row_num=template_params['row_num'],
                             data=template_params['data'], data_len=template_params['data_len']))
+
+# generate dictionary for statistics 1
+def gen_statistics_1(file):
+    df = file.agg(['mean', 'median', 'std', 'var'])
+    template_params = {
+        'title': 'Статистический анализ 1',
+        'col_title': list(df.columns),
+        'row_num': [['mean', 0], ['median', 1], ['std', 2], ['var', 3]],
+        'data': df.values.tolist(),
+        'data_len': [i for i in range(0, len(df.columns))]
+    }
+    return template_params
+
+# generate dictionary for statistics 2
+def gen_statistics_2(file):
+    df_eng = file[file['1.3.1'] == True].agg(['mean', 'median', 'std', 'var'])
+    df_ru = file[file['1.3.1'] == False].agg(['mean', 'median', 'std', 'var'])
+    col_title = []
+    if len(df_eng.columns) != 0:
+        col_title = list(df_eng.columns)
+    else:
+        col_title = list(df_ru.columns)
+    template_params = {
+        'title': 'Статистический анализ 2',
+        'col_title': col_title,
+        'row_num': [['mean_eng', 0], ['median_eng', 1], ['std_eng', 2], ['var_eng', 3], ['mean_ru', 4], ['median_ru', 5], ['std_ru', 6], ['var_ru', 7]],
+        'data': df_eng.values.tolist() + df_ru.values.tolist(),
+        'data_len': [i for i in range(0, len(col_title))]
+    }
+    return template_params
+
 # handle statistics
 def work_with_format_csv(path_to_csv):
     temp_path = ""
@@ -36,13 +67,8 @@ def work_with_format_csv(path_to_csv):
     path_to_csv = temp_path
     file = pd.read_csv(path_to_csv, sep=';')
     file = file.drop(file.columns[0], axis=1)
-    df = file.agg(['mean', 'median', 'std', 'var'])
     path = path_to_csv[:(path_to_csv.rfind('/')) + 1]
-    template_params = {
-        'title': ['Статистический анализ 1'],
-        'col_title': list(df.columns),
-        'row_num': [['mean', 0], ['median', 1], ['std', 2], ['var', 3]],
-        'data': df.values.tolist(),
-        'data_len': [i for i in range(0, len(df.columns))]
-    }
-    generate_html('Статистический анализ 1', path, template_params)
+    template_params = gen_statistics_1(file)
+    generate_html(template_params['title'], path, template_params)
+    template_params = gen_statistics_2(file)
+    generate_html(template_params['title'], path, template_params)

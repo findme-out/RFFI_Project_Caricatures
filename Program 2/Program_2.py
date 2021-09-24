7aefbf4dd02e556d5a547836a09e38686151fc2a
findme-out <89270561+findme-out@users.noreply.github.com>
2021-09-24 18:02:36 +0300

    Добавлена функция генерации статистики
    Добавлена генерация статистики "Статистический анализ 3"

--- Program 2/Program_2.py
+++ Program 2/Program_2.py
@@ -56,6 +56,36 @@ def gen_statistics_2(file):
     }
     return template_params
 
+# generate dictionary for statistics 3
+def gen_statistics_3(file):
+    df_eng = file[file['1.3.1'] == True]
+    df_eng_1 = df_eng[df_eng['2.1.1'] == True].agg(['mean', 'median', 'std', 'var'])
+    df_eng_2 = df_eng[df_eng['2.1.2'] == True].agg(['mean', 'median', 'std', 'var'])
+    df_eng_3 = df_eng[df_eng['2.1.3'] == True].agg(['mean', 'median', 'std', 'var'])
+    df_eng_4 = df_eng[df_eng['2.1.4'] == True].agg(['mean', 'median', 'std', 'var'])
+    df_ru = file[file['1.3.1'] == False]
+    df_ru_1 = df_ru[df_ru['2.1.1'] == True].agg(['mean', 'median', 'std', 'var'])
+    df_ru_2 = df_ru[df_ru['2.1.2'] == True].agg(['mean', 'median', 'std', 'var'])
+    df_ru_3 = df_ru[df_ru['2.1.3'] == True].agg(['mean', 'median', 'std', 'var'])
+    df_ru_4 = df_ru[df_ru['2.1.4'] == True].agg(['mean', 'median', 'std', 'var'])
+    col_title = []
+    if len(df_eng.columns) != 0:
+        col_title = list(df_eng.columns)
+    else:
+        col_title = list(df_ru.columns)
+    template_params = {
+        'title': 'Статистический анализ 3',
+        'col_title': col_title,
+        'row_num': [['mean_eng_1', 0], ['median_eng_1', 1], ['std_eng_1', 2], ['var_eng_1', 3], ['mean_ru_1', 4], ['median_ru_1', 5], ['std_ru_1', 6], ['var_ru_1', 7],
+                    ['mean_eng_2', 8], ['median_eng_2', 9], ['std_eng_2', 10], ['var_eng_2', 11], ['mean_ru_2', 12], ['median_ru_2', 13], ['std_ru_2', 14], ['var_ru_2', 15],
+                    ['mean_eng_3', 16], ['median_eng_3', 17], ['std_eng_3', 18], ['var_eng_3', 19], ['mean_ru_3', 20], ['median_ru_3', 21], ['std_ru_3', 22], ['var_ru_3', 23],
+                    ['mean_eng_4', 24], ['median_eng_4', 25], ['std_eng_4', 26], ['var_eng_4', 27], ['mean_ru_4', 28], ['median_ru_4', 29], ['std_ru_4', 30], ['var_ru_4', 31]],
+        'data': df_eng_1.values.tolist() + df_ru_1.values.tolist() + df_eng_2.values.tolist() + df_ru_2.values.tolist() +
+        df_eng_3.values.tolist() + df_ru_3.values.tolist() + df_eng_4.values.tolist() + df_ru_4.values.tolist(),
+        'data_len': [i for i in range(0, len(col_title))]
+    }
+    return template_params
+
 # handle statistics
 def work_with_format_csv(path_to_csv):
     temp_path = ""
@@ -71,4 +101,6 @@ def work_with_format_csv(path_to_csv):
     template_params = gen_statistics_1(file)
     generate_html(template_params['title'], path, template_params)
     template_params = gen_statistics_2(file)
+    generate_html(template_params['title'], path, template_params)
+    template_params = gen_statistics_3(file)
     generate_html(template_params['title'], path, template_params)

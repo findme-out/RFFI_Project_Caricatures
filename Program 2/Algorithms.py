f75862e0fb0f8e62da38c555767d95edc75be68b
findme-out <89270561+findme-out@users.noreply.github.com>
2021-10-09 08:09:57 +0300

    Add files via upload

--- Program 2/Algorithms.py
+++ Program 2/Algorithms.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from scipy.stats import chi2_contingency
+import numpy as np
+import matplotlib.pyplot as plt
 
 # utility function
 def add_one(x):
@@ -13,7 +15,7 @@ def gen_stat(df, expected):
     for i in range(0, len(first_vals)):
         temp = []
         for j in range(0, len(first_vals[i])):
-            temp.append(first_vals[i][j] / last_vals[i][j] * 100)
+            temp.append(round(100 - first_vals[i][j] / last_vals[i][j] * 100, 4))
         res.append(temp)
     return res
         
@@ -26,6 +28,17 @@ def gen_statistics_1(file):
     stat, p, dof, expected = chi2_contingency(df)
     res = gen_stat(df, expected)
     res = pd.DataFrame([res[0], res[1]], index=['eng', 'ru'], columns=df.columns)
+    ax = plt.figure(figsize=(120,3))
+    X = res.columns.tolist()
+    row_0 = res.iloc[0].values.tolist()
+    row_1 = res.iloc[1].values.tolist()
+    X_axis = np.arange(len(X))
+    for Y, Y_desc in [[row_0, 'eng'], [row_1, 'ru']]:
+        plt.bar(X_axis, Y, label=Y_desc, align='center', width=0.6)
+    plt.xticks(X_axis, X)
+    plt.xlabel("Rules")
+    plt.ylabel("Percentage difference")
+    plt.legend(loc='upper left')
     template_params = {
         'desc': '',
         'col_title': res.columns.tolist(),
@@ -38,7 +51,7 @@ def gen_statistics_1(file):
         'desc': 'Тест значимости при помощи алгоритма chi-square',
         'value': [template_params]
     }
-    return res_dict
+    return res_dict, [ax]
 
 # generate dictionary for statistics 2
 def gen_statistics_2(file):
@@ -49,6 +62,17 @@ def gen_statistics_2(file):
     stat, p, dof, expected = chi2_contingency(df)
     res = gen_stat(df, expected)
     res = pd.DataFrame([res[0], res[1]], index=['eng', 'ru'], columns=df.columns)
+    ax = plt.figure(figsize=(120, 3))
+    X = res.columns.tolist()
+    row_0 = res.iloc[0].values.tolist()
+    row_1 = res.iloc[1].values.tolist()
+    X_axis = np.arange(len(X))
+    for Y, Y_desc in [[row_0, 'eng'], [row_1, 'ru']]:
+        plt.bar(X_axis, Y, label=Y_desc, align='center', width=0.6)
+    plt.xticks(X_axis, X)
+    plt.xlabel("Rules")
+    plt.ylabel("Percentage difference")
+    plt.legend(loc='upper left')
     template_params = {
         'desc': 'Разделение на языковые подгруппы согласно пункту 1.3.1 (eng-ru)',
         'col_title': res.columns.tolist(),
@@ -61,10 +85,12 @@ def gen_statistics_2(file):
         'desc': 'Тест значимости при помощи алгоритма chi-square',
         'value': [template_params]
     }
-    return res_dict
+    return res_dict, [ax]
 
 # generate dictionary for statistics 3
 def gen_statistics_3(file):
+    df_eng = file[file['1.3.1'] == True]
+    df_ru = file[file['1.3.1'] == False]
     df_1 = file[file['2.1.1']==True]
     df_2 = file[file['2.1.2']==True]
     df_3 = file[file['2.1.3']==True]
@@ -74,23 +100,37 @@ def gen_statistics_3(file):
         'desc': 'Тест значимости при помощи алгоритма chi-square на языковых подгруппах согласно пункту 1.3.1 (eng-ru)',
         'value': []
     }
-    for data, desc_num in [[df_1, 'Разделение подгруппы по фоновым знаниям согласно пункту 2.1.1'], 
-                            [df_2, 'Разделение на подгруппы по фоновым знаниям согласно пункту 2.1.2'], 
-                            [df_3, 'Разделение на подгруппы по фоновым знаниям согласно пункту 2.1.3'], 
-                            [df_4, 'Разделение на подгруппы по фоновым знаниям согласно пункту 2.1.4']]:
-        data = pd.DataFrame([data[data['1.3.1'] == True].sum(), data[data['1.3.1'] == False].sum()], index=['eng', 'ru'])
-        data = data.drop(['1.3.1', '1.3.2'], axis=1)
-        if 0 in data.values:
-            data = data.apply(add_one)
-        stat, p, dof, expected = chi2_contingency(data)
-        res = gen_stat(data, expected)
-        res = pd.DataFrame([res[0], res[1]], index=['eng', 'ru'], columns=data.columns)
-        template_params = {
-            'desc': desc_num,
-            'col_title': res.columns.tolist(),
-            'row_num': [[res.index.tolist()[i], i] for i in range(0, len(res.index.tolist()))],
-            'data': res.values.tolist(),
-            'data_len': [i for i in range(0, len(res.columns))]
-        }
-        res_dict['value'].append(template_params)
-    return res_dict
+    axes = []
+    for data, desc_num, data_point in [[df_1, 'Разделение подгруппы по фоновым знаниям согласно пункту 2.1.1', '2.1.1'],
+                            [df_2, 'Разделение на подгруппы по фоновым знаниям согласно пункту 2.1.2', '2.1.2'],
+                            [df_3, 'Разделение на подгруппы по фоновым знаниям согласно пункту 2.1.3', '2.1.3'],
+                            [df_4, 'Разделение на подгруппы по фоновым знаниям согласно пункту 2.1.4', '2.1.4']]:
+        for second_data, second_data_desc in [[df_eng, 'eng'], [df_ru, 'ru']]:
+            data = pd.DataFrame([second_data.sum(), data.sum()], index=[second_data_desc, data_point])
+            data = data.drop(['1.3.1', '1.3.2'], axis=1)
+            if 0 in data.values:
+                data = data.apply(add_one)
+            stat, p, dof, expected = chi2_contingency(data)
+            res = gen_stat(data, expected)
+            res = pd.DataFrame([res[0], res[1]], index=[second_data_desc, data_point], columns=data.columns)
+            ax = plt.figure(figsize=(120, 3))
+            X = res.columns.tolist()
+            row_0 = res.iloc[0].values.tolist()
+            row_1 = res.iloc[1].values.tolist()
+            X_axis = np.arange(len(X))
+            for Y, Y_desc in [[row_0, second_data_desc], [row_1, data_point]]:
+                plt.bar(X_axis, Y, label=Y_desc, align='center', width=0.6)
+            plt.xticks(X_axis, X)
+            plt.xlabel("Rules")
+            plt.ylabel("Percentage difference")
+            plt.legend(loc='upper left')
+            template_params = {
+                'desc': desc_num + ', ' + second_data_desc,
+                'col_title': res.columns.tolist(),
+                'row_num': [[res.index.tolist()[i], i] for i in range(0, len(res.index.tolist()))],
+                'data': res.values.tolist(),
+                'data_len': [i for i in range(0, len(res.columns))]
+            }
+            res_dict['value'].append(template_params)
+            axes.append(ax)
+    return res_dict, axes
--- Program 2/Program_2.py
+++ Program 2/Program_2.py
@@ -2,6 +2,7 @@ import Algorithms
 import os
 import pandas as pd
 from chameleon import PageTemplateLoader
+import base64
 
 # generate html file according to the template
 def generate_html(path_to_csv, template_list, template_name='template.html', output_file_name='Алгоритмы'):
@@ -41,9 +42,39 @@ def work_with_format_csv(path_to_csv, gen_statistics_1=True, gen_statistics_2=Tr
     path = path_to_csv[:(path_to_csv.rfind('/')) + 1]
     template_list = []
     if gen_statistics_1:
-        template_list.append(Algorithms.gen_statistics_1(file))
+        temp_dict, fig_list = Algorithms.gen_statistics_1(file)
+        temp_path = path + 'stat_1_'
+        for i in range(0, len(fig_list)):
+            temp_temp_path = temp_path + str(i) + '.png'
+            fig_list[i].savefig(temp_temp_path)
+            with open(temp_temp_path, 'rb') as image_file:
+                encoded_string = base64.b64encode(image_file.read())
+            fig_list[i] = encoded_string
+        for i in range(0, len(temp_dict['value'])):
+            temp_dict['value'][i]['graph'] = fig_list[i]
+        template_list.append(temp_dict)
     if gen_statistics_2:
-        template_list.append(Algorithms.gen_statistics_2(file))
+        temp_dict, fig_list = Algorithms.gen_statistics_2(file)
+        temp_path = path + 'stat_2_'
+        for i in range(0, len(fig_list)):
+            temp_temp_path = temp_path + str(i) + '.png'
+            fig_list[i].savefig(temp_temp_path)
+            with open(temp_temp_path, 'rb') as image_file:
+                encoded_string = base64.b64encode(image_file.read())
+            fig_list[i] = encoded_string
+        for i in range(0, len(temp_dict['value'])):
+            temp_dict['value'][i]['graph'] = fig_list[i]
+        template_list.append(temp_dict)
     if gen_statistics_3:
-        template_list.append(Algorithms.gen_statistics_3(file))
+        temp_dict, fig_list = Algorithms.gen_statistics_3(file)
+        temp_path = path + 'stat_3_'
+        for i in range(0, len(fig_list)):
+            temp_temp_path = temp_path + str(i) + '.png'
+            fig_list[i].savefig(temp_temp_path)
+            with open(temp_temp_path, 'rb') as image_file:
+                encoded_string = base64.b64encode(image_file.read())
+            fig_list[i] = encoded_string
+        for i in range(0, len(temp_dict['value'])):
+            temp_dict['value'][i]['graph'] = fig_list[i]
+        template_list.append(temp_dict)
     generate_html(path, template_list)

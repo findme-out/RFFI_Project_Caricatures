535098589f691bbf63035893b9cb468ce1aff388
findme-out <89270561+findme-out@users.noreply.github.com>
2021-10-18 17:07:37 +0300

    Add files via upload
    + Небольшие изменения в дизайне (разделительная полоса в клетках)
    + Исправлен 3 алгоритм статистики согласно замечаниям
    + Перевод на русский язык

--- Program 2/Algorithms.py
+++ Program 2/Algorithms.py
@@ -28,17 +28,25 @@ def gen_statistics_1(file):
     stat, p, dof, expected = chi2_contingency(df)
     res = gen_stat(df, expected)
     res = pd.DataFrame([res[0], res[1]], index=['eng', 'ru'], columns=df.columns)
-    ax = plt.figure(figsize=(120,3))
+    ax = plt.figure(figsize=(40, 5))
     X = res.columns.tolist()
     row_0 = res.iloc[0].values.tolist()
     row_1 = res.iloc[1].values.tolist()
     X_axis = np.arange(len(X))
+    bar_width = 0.4
     for Y, Y_desc in [[row_0, 'eng'], [row_1, 'ru']]:
-        plt.bar(X_axis, Y, label=Y_desc, align='center', width=0.6)
-    plt.xticks(X_axis, X)
-    plt.xlabel("Rules")
-    plt.ylabel("Percentage difference")
+        plt.bar(X_axis, Y, label=Y_desc, align='center', width=bar_width, edgecolor='grey')
+        X_axis = [x + bar_width for x in X_axis]
+    plt.xticks([r + bar_width for r in range(len(X))], X, rotation=45)
+    plt.xlabel("Правила")
+    plt.ylabel("Разница в %")
     plt.legend(loc='upper left')
+    plt.tight_layout()
+    temp_data = res.values.tolist()
+    for i in range(0, len(temp_data)):
+        for j in range(0, len(temp_data[i])):
+            temp_data[i][j] = [str(round(p, 4)), str(temp_data[i][j])]
+    res = pd.DataFrame([temp_data[0], temp_data[1]], index=['eng', 'ru'], columns=res.columns)
     template_params = {
         'desc': '',
         'col_title': res.columns.tolist(),
@@ -62,17 +70,25 @@ def gen_statistics_2(file):
     stat, p, dof, expected = chi2_contingency(df)
     res = gen_stat(df, expected)
     res = pd.DataFrame([res[0], res[1]], index=['eng', 'ru'], columns=df.columns)
-    ax = plt.figure(figsize=(120, 3))
+    ax = plt.figure(figsize=(40, 5))
     X = res.columns.tolist()
     row_0 = res.iloc[0].values.tolist()
     row_1 = res.iloc[1].values.tolist()
     X_axis = np.arange(len(X))
+    bar_width = 0.4
     for Y, Y_desc in [[row_0, 'eng'], [row_1, 'ru']]:
-        plt.bar(X_axis, Y, label=Y_desc, align='center', width=0.6)
-    plt.xticks(X_axis, X)
-    plt.xlabel("Rules")
-    plt.ylabel("Percentage difference")
+        plt.bar(X_axis, Y, label=Y_desc, align='center', width=bar_width, edgecolor='grey')
+        X_axis = [x + bar_width for x in X_axis]
+    plt.xticks([r + bar_width for r in range(len(X))], X, rotation=45)
+    plt.xlabel("Правила")
+    plt.ylabel("Разница в %")
     plt.legend(loc='upper left')
+    plt.tight_layout()
+    temp_data = res.values.tolist()
+    for i in range(0, len(temp_data)):
+        for j in range(0, len(temp_data[i])):
+            temp_data[i][j] = [str(round(p, 4)), str(temp_data[i][j])]
+    res = pd.DataFrame([temp_data[0], temp_data[1]], index=['eng', 'ru'], columns=res.columns)
     template_params = {
         'desc': 'Разделение на языковые подгруппы согласно пункту 1.3.1 (eng-ru)',
         'col_title': res.columns.tolist(),
@@ -91,46 +107,55 @@ def gen_statistics_2(file):
 def gen_statistics_3(file):
     df_eng = file[file['1.3.1'] == True]
     df_ru = file[file['1.3.1'] == False]
-    df_1 = file[file['2.1.1']==True]
-    df_2 = file[file['2.1.2']==True]
-    df_3 = file[file['2.1.3']==True]
-    df_4 = file[file['2.1.4']==True]
     res_dict = {
         'title': 'Статистический анализ 3',
         'desc': 'Тест значимости при помощи алгоритма chi-square на языковых подгруппах согласно пункту 1.3.1 (eng-ru)',
         'value': []
     }
     axes = []
-    for data, desc_num, data_point in [[df_1, 'Разделение подгруппы по фоновым знаниям согласно пункту 2.1.1', '2.1.1'],
-                            [df_2, 'Разделение на подгруппы по фоновым знаниям согласно пункту 2.1.2', '2.1.2'],
-                            [df_3, 'Разделение на подгруппы по фоновым знаниям согласно пункту 2.1.3', '2.1.3'],
-                            [df_4, 'Разделение на подгруппы по фоновым знаниям согласно пункту 2.1.4', '2.1.4']]:
-        for second_data, second_data_desc in [[df_eng, 'eng'], [df_ru, 'ru']]:
-            data = pd.DataFrame([second_data.sum(), data.sum()], index=[second_data_desc, data_point])
-            data = data.drop(['1.3.1', '1.3.2'], axis=1)
-            if 0 in data.values:
-                data = data.apply(add_one)
-            stat, p, dof, expected = chi2_contingency(data)
-            res = gen_stat(data, expected)
-            res = pd.DataFrame([res[0], res[1]], index=[second_data_desc, data_point], columns=data.columns)
-            ax = plt.figure(figsize=(120, 3))
-            X = res.columns.tolist()
-            row_0 = res.iloc[0].values.tolist()
-            row_1 = res.iloc[1].values.tolist()
-            X_axis = np.arange(len(X))
-            for Y, Y_desc in [[row_0, second_data_desc], [row_1, data_point]]:
-                plt.bar(X_axis, Y, label=Y_desc, align='center', width=0.6)
-            plt.xticks(X_axis, X)
-            plt.xlabel("Rules")
-            plt.ylabel("Percentage difference")
-            plt.legend(loc='upper left')
-            template_params = {
-                'desc': desc_num + ', ' + second_data_desc,
-                'col_title': res.columns.tolist(),
-                'row_num': [[res.index.tolist()[i], i] for i in range(0, len(res.index.tolist()))],
-                'data': res.values.tolist(),
-                'data_len': [i for i in range(0, len(res.columns))]
-            }
-            res_dict['value'].append(template_params)
-            axes.append(ax)
+    for data, desc_num in [[df_eng, 'Разделение подгруппы по фоновым знаниям согласно пункту 2.1.1, 2.1.2, 2.1.3, 2.1.4 на данных подгруппы eng'],
+                            [df_ru, 'Разделение подгруппы по фоновым знаниям согласно пункту 2.1.1, 2.1.2, 2.1.3, 2.1.4 на данных подгруппы ru']]:
+        df_1 = data[data['2.1.1']==True]
+        df_2 = data[data['2.1.2']==True]
+        df_3 = data[data['2.1.3']==True]
+        df_4 = data[data['2.1.4']==True]
+        data_temp = pd.DataFrame([df_1.sum(), df_2.sum(), df_3.sum(), df_4.sum()], index=['2.1.1', '2.1.2', '2.1.3', '2.1.4'])
+        data_temp = data_temp.drop(['1.3.1', '1.3.2', '2.1.1', '2.1.2', '2.1.3', '2.1.4'], axis=1)
+        if 0 in data_temp.values:
+            data_temp = data_temp.apply(add_one)
+        stat, p, dof, expected = chi2_contingency(data_temp)
+        res = gen_stat(data_temp, expected)
+        res = pd.DataFrame([res[0], res[1], res[2], res[3]], index=['2.1.1', '2.1.2', '2.1.3', '2.1.4'], columns=data_temp.columns)
+        ax = plt.figure(figsize=(40, 5))
+        X = res.columns.tolist()
+        row_0 = res.iloc[0].values.tolist()
+        row_1 = res.iloc[1].values.tolist()
+        row_2 = res.iloc[2].values.tolist()
+        row_3 = res.iloc[3].values.tolist()
+        X_axis = np.arange(len(X))
+        bar_width = 0.2
+        temp = 0
+        for Y, Y_desc in [[row_0, '2.1.1'], [row_1, '2.1.2'], [row_2, '2.1.3'], [row_3, '2.1.4']]:
+            #X_axis = [x + temp for x in X_axis]
+            plt.bar(X_axis + temp, Y, label=Y_desc, align='center', width=bar_width, edgecolor='grey')
+            temp += bar_width
+        plt.xticks([r + bar_width for r in range(len(X))], X, rotation=45)
+        plt.xlabel("Правила")
+        plt.ylabel("Разница в %")
+        plt.legend(loc='upper left')
+        plt.tight_layout()
+        temp_data = res.values.tolist()
+        for i in range(0, len(temp_data)):
+            for j in range(0, len(temp_data[i])):
+                temp_data[i][j] = [str(round(p, 4)), str(temp_data[i][j])]
+        res = pd.DataFrame([temp_data[0], temp_data[1], temp_data[2], temp_data[3]], index=['2.1.1', '2.1.2', '2.1.3', '2.1.4'], columns=res.columns)
+        template_params = {
+            'desc': desc_num,
+            'col_title': res.columns.tolist(),
+            'row_num': [[res.index.tolist()[i], i] for i in range(0, len(res.index.tolist()))],
+            'data': res.values.tolist(),
+            'data_len': [i for i in range(0, len(res.columns))]
+        }
+        res_dict['value'].append(template_params)
+        axes.append(ax)
     return res_dict, axes
